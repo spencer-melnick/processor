89b8fee63be5630f5d1cc508456fa4c14c997562
spencer-melnick <smelnick97@gmail.com>
2018-11-17 12:44:51 -0500

    Updating testbenches

--- adder_32_tb.v
+++ adder_32_tb.v
@@ -12,7 +12,11 @@ module adder_32_tb;
 	wire c_out;
 	
 	// Temporaries
+	reg [3:0] i;
 	reg [63:0] errors;
+
+	// Test vectors
+	reg [103:0] test [7:0];
 
 	// Instantiate the Unit Under Test (UUT)
 	adder_32 uut (
@@ -24,58 +28,39 @@ module adder_32_tb;
 	);
 
 	initial begin
+		$dumpfile("adder_32.vcd");
+		$dumpvars;
+
 		// Initialize Inputs
 		a = 0;
 		b = 0;
 		c_in = 0;
+
+		// Load test values
+		$readmemh("../tv/adder_32_tv.tv", test);
+		i = 0;
 		errors = 0;
 
 		// Wait 10 ns for global reset to finish
 		#10;
         
-		// Add stimulus here
-		a = 32'h7fffffff;
-		b = 32'h7fffffff;
-		c_in = 1;
-					
-		#5;
-					
-		if ((s !== 32'hffffffff) || (c_out !== 0)) begin
-			$display("Error: inputs = %d %d %b", a[31:0], b[31:0], c_in);
-			$display("		 outputs = %d %b", s, c_out);
-			$display("		expected = %d %b", 32'hffffffff, 0);
-						
-			errors = errors + 1;
-		end
-		
-		
-		a = 0;
-		b = 16;
-		c_in = 0;
-					
-		#5;
-					
-		if ((s !== 16) || (c_out !== 0)) begin
-			$display("Error: inputs = %d %d %b", a[31:0], b[31:0], c_in);
-			$display("		 outputs = %d %b", s, c_out);
-			$display("		expected = %d %b", 16, 0);
-						
-			errors = errors + 1;
-		end
-		
-		
-		a = 32'hffffffff;
-		b = 32'hffffffff;
-		c_in = 1;
-					
-		#5;
-					
-		if ((s !== 32'hffffffff) || (c_out !== 1)) begin
-			$display("Error: inputs = %d %d %b", a[31:0], b[31:0], c_in);
-			$display("		 outputs = %d %b", s, c_out);
-			$display("		expected = %d %b", 32'hffffffff, 0);
-						
-			errors = errors + 1;
+		while (test[i] !== 104'bx) begin
+			// Apply inputs
+			a = test[i][103:72];
+			b = test[i][71:40];
+			c_in = test[i][36];
+			
+			#5;
+
+			if (s != test[i][35:4] || c_out != test[i][0]) begin
+				errors = errors + 1;
+				$display("Error on input %d", i);
+				$display("          Input values: %d %d %d", a, b, c_in);
+				$display("         Output values: %d %d", s, c_out);
+				$display("Expected output values: %d %d", test[i][35:4], test[i][0]);
+			end
+
+			i = i + 1;
 		end
 		
 		if (errors == 0) begin
